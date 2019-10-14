a6eb4863b670a8dabe072c1c102fbc8bf63f81d0
Peter Crefeld <pjc509@nyu.edu>
2019-10-14 12:05:00 -0400

    updates to forms

--- forms.py
+++ forms.py
@@ -30,6 +30,7 @@ class RegisterForm(Form):
                         DataRequired(),
                         EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    p2fa = PasswordField('2fa',id="2fa", validators=[DataRequired()])
     email = StringField('Email',
                         validators=[DataRequired(), Length(1, 120), Email()])
 
