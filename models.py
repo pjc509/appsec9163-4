866f0f0edaef112217cdba10b1a01e4868df151a
Peter Crefeld <pjc509@nyu.edu>
2019-10-17 00:20:32 -0400

    updated register form

--- models.py
+++ models.py
@@ -23,7 +23,7 @@ db = SQLAlchemy(app)
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120))
     password_hash = db.Column(db.String)
 
     @property
